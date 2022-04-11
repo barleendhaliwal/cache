c9b677eb766d3d600f62639ca9303ba565a0f8b8
Barleen  Dhaliwal <barleen.dhaliwal@sourcefuse.com>
2022-04-11 13:22:49 +0530

    replace promises with async/await

--- src/mixins/cache.mixin.ts
+++ src/mixins/cache.mixin.ts
@@ -31,18 +31,16 @@ export function CacheRespositoryMixin<
     ): Promise<M> {
       this.checkDataSource();
       const key = this.getKey(id, filter, options);
-
-      return this.searchInCache(key).then(async result => {
-        let finalResult;
-        if (result) {
-          finalResult = result;
-        } else {
-          const dbEntry = await super.findById(id, filter, options);
-          finalResult = dbEntry;
-          this.saveInCache(key, dbEntry);
-        }
-        return finalResult as M;
-      });
+      const result = await this.searchInCache(key);
+      let finalResult;
+      if (result) {
+        finalResult = result;
+      } else {
+        const dbEntry = await super.findById(id, filter, options);
+        finalResult = dbEntry;
+        this.saveInCache(key, dbEntry);
+      }
+      return finalResult as M;
     }
 
     /* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
@@ -53,53 +51,45 @@ export function CacheRespositoryMixin<
     ): Promise<M[]> {
       this.checkDataSource();
       const key = this.getKey(undefined, filter, options);
-
-      return this.searchInCache(key).then(async result => {
-        let finalResult;
-        if (result) {
-          finalResult = result;
-        } else {
-          const dbEntry = await super.find(filter, options);
-          finalResult = dbEntry;
-          this.saveInCache(key, dbEntry);
-        }
-        return finalResult as M[];
-      });
+      const result = await this.searchInCache(key);
+      let finalResult;
+      if (result) {
+        finalResult = result;
+      } else {
+        const dbEntry = await super.find(filter, options);
+        finalResult = dbEntry;
+        this.saveInCache(key, dbEntry);
+      }
+      return finalResult as M[];
     }
 
-    searchInCache(key: string): Promise<M | M[] | undefined> {
-      return new Promise((resolve, reject) => {
-        if (this.cacheDataSource.connector?.execute) {
-          this.cacheDataSource.connector.execute(
-            'GET',
-            [key],
-            (err: Error, res: Buffer) => {
-              if (err) {
-                reject(err);
-              }
-              if (res) {
-                const result = JSON.parse(decoder.decode(res));
-                resolve(result);
-              } else {
-                return resolve(undefined);
-              }
-            },
-          );
+    async searchInCache(key: string) {
+      let result = undefined;
+      if (this.cacheDataSource.connector?.execute) {
+        try {
+          const res = await this.executeRedisCommand('GET', [key]);
+          if (res) {
+            result = JSON.parse(decoder.decode(res as ArrayBuffer));
+          }
+        } catch (err) {
+          throw new Error(`Unable to search in Cache : ${err}`);
         }
-      });
+        return result;
+      }
     }
 
     saveInCache(key: string, value: AnyObject) {
       if (this.cacheDataSource.connector?.execute) {
-        this.cacheDataSource.connector?.execute(
-          'SET',
-          [key, JSON.stringify(value), `PX`, cacheOptions.ttl ?? 60000],
-          (err: Error) => {
-            if (err) {
-              throw new Error(`Unable to save in Cache ${err}`);
-            }
-          },
-        );
+        try {
+          this.executeRedisCommand('SET', [
+            key,
+            JSON.stringify(value),
+            `PX`,
+            cacheOptions.ttl ?? 60000,
+          ]);
+        } catch (err) {
+          throw new Error(`Unable to save in Cache : ${err}`);
+        }
       }
     }
 
@@ -108,7 +98,6 @@ export function CacheRespositoryMixin<
       if (id) {
         key += `_${id}`;
       }
-
       if (filter) {
         key += `_${JSON.stringify(filter)}`;
       }
@@ -119,81 +108,93 @@ export function CacheRespositoryMixin<
     }
 
     //returns number of keys deleted
-    clearCache() {
+    async clearCache() {
       this.checkDataSource();
-      return new Promise((resolve, reject) => {
-        this.getMatchingKeys().then(result => {
-          const keys = result as Buffer[];
-          if (this.cacheDataSource.connector?.execute && keys.length) {
-            this.cacheDataSource.connector?.execute(
-              'DEL',
-              [keys],
-              (err: Error, result: number) => {
-                if (err) {
-                  reject(err);
-                }
-                resolve(result);
-              },
-            );
-          } else {
-            resolve(0);
-          }
-        });
-      });
+      const result = await this.getMatchingKeys() as string[];
+      if (this.cacheDataSource.connector?.execute && result.length) {
+        const count = await this.executeRedisCommand('DEL', result);
+        return count;
+      } else {
+        return 0;
+      }
     }
 
+    // returns all keys that match with prefix using SCAN command.
+    // need to write it like this because cursor is updated asynchronously. reference: https://stackoverflow.com/questions/43064719/javascript-asynchronous-method-in-while-loop
     getMatchingKeys() {
       return new Promise((resolve, reject) => {
         this.scanAllKeys(resolve, reject);
       });
     }
 
-    scanAllKeys(
+    //scans and returns all keys till cursor becomes zero
+    async scanAllKeys(
       resolve: any,
       reject: any,
       cursor: number = 0,
       keys: string[] = [],
     ) {
-      this.scanKeys(cursor).then((res: {cursor: number; keys: string[]}) => {
-        cursor = res.cursor;
-        keys.push(...res.keys);
-        if (cursor === 0) {
-          resolve(keys);
-        } else {
-          this.scanAllKeys(resolve, reject, cursor, keys);
-        }
-      });
+      const res: {cursor: number; keys: string[]} = await this.scanKeys(cursor);
+      cursor = res.cursor;
+      keys.push(...res.keys);
+      if (cursor === 0) {
+        resolve(keys);
+      } else {
+        await this.scanAllKeys(resolve, reject, cursor, keys);
+      }
     }
-    scanKeys(cursor: number): Promise<{cursor: number; keys: string[]}> {
-      return new Promise((resolve, reject) => {
-        if (this.cacheDataSource.connector?.execute) {
-          this.cacheDataSource.connector?.execute(
-            'SCAN',
-            [cursor, 'MATCH', `${cacheOptions.prefix}*`],
-            (err: Error, res: any) => {
-              if (err) {
-                reject(err);
-              } else {
-                const keys: string[] = [];
-                res[1].forEach((key: Buffer) => {
-                  keys.push(decoder.decode(key));
-                });
-                const result = {
-                  cursor: parseInt(decoder.decode(res[0])),
-                  keys,
-                };
-                resolve(result);
-              }
-            },
-          );
-        }
-      });
+
+    async scanKeys(cursor: number): Promise<{cursor: number; keys: string[]}> {
+      try {
+        const res: any = await this.executeRedisCommand('SCAN', [
+          cursor,
+          'MATCH',
+          `${cacheOptions.prefix}*`,
+          'COUNT',
+          cacheOptions.scanCount ?? 100,
+        ]);
+        const keys: string[] = [];
+        res[1].forEach((key: Buffer) => {
+          keys.push(decoder.decode(key));
+        });
+        const result = {
+          cursor: parseInt(decoder.decode(res[0])),
+          keys,
+        };
+        return result;
+      } catch (err) {
+        throw new Error(
+          `Unable to get matching keys for clearing cache ${err}`,
+        );
+      }
     }
 
     checkDataSource() {
       if (!this.cacheDataSource) {
         throw new Error(`Please provide value for cacheDataSource`);
       }
+    }
+
+    // returns promisified execute function
+    executeRedisCommand(command: string, args: (string | number)[]) {
+      return new Promise((resolve, reject) => {
+        if (this.cacheDataSource.connector?.execute) {
+          this.cacheDataSource.connector.execute(
+            command,
+            args,
+            (err: Error, res: Buffer) => {
+              if (err) {
+                reject(err);
+              }
+              if (res) {
+                resolve(res);
+              } else {
+                return resolve(undefined);
+              }
+            },
+          );
+        }
+      });
     }
   }
   return MixedRepository;
